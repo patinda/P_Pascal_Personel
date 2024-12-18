03903c9bdda8d97802508f7df45f673722303551
patinda7 <pkabore@futurion.tech>
2024-12-18 01:11:53 +0000

    v5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,49 +11,25 @@ export default defineNuxtConfig({
       { code: "fr", name: "Français", file: "fr.json" },
     ],
     defaultLocale: "en",
-    langDir: "locales", 
+    langDir: "locales",
     strategy: "prefix_except_default",
   },
-  css: [
-    'assets/css/swiper-bundle.min.css',
-    'assets/css/styles.css',
-  ],
   app: {
-    
     head: {
       link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://unicons.iconscout.com/release/v4.0.0/css/line.css',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://res.cloudinary.com/dkztnapze/raw/upload/v1734475902/portfolio/swiper-bundle.min_jh216d.css',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://res.cloudinary.com/dkztnapze/raw/upload/v1734475901/portfolio/styles_zwxl2s.css',
-        },
-        
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' },
+        { rel: 'stylesheet', href: 'https://unicons.iconscout.com/release/v4.0.0/css/line.css' },
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' },
+        { rel: 'stylesheet', href: 'https://res.cloudinary.com/dkztnapze/raw/upload/v1734475901/portfolio/styles_zwxl2s.css' },
       ],
       script: [
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+          defer: true,
         },
         {
-          src: '/js/main.js',
+          src: 'https://res.cloudinary.com/dkztnapze/raw/upload/v1734476268/portfolio/main_eyfkoy.js',
+          defer: true,
         },
       ],
     },
